7511a1d9e8e8736148cb0903bd7f3cadf32c5dcc
Brett Hughes <twofivinclimber@gmail.com>
2022-04-19 21:34:36 -0500

    added event listener to button

--- main.js
+++ main.js
@@ -18,29 +18,37 @@ const team = [
     name: "Dr. T",
     email: "drt@drt.com",
     favoriteColor: "red",
-    image: "https://avatars3.githubusercontent.com/u/29741570?s=460&u=16cc038bd4c459a12eb198fc313043cd37c80a64&v=4"
+    image:
+      "https://avatars3.githubusercontent.com/u/29741570?s=460&u=16cc038bd4c459a12eb198fc313043cd37c80a64&v=4",
   },
   {
     id: 2,
     name: "Aja",
     email: "drt@drt.com",
     favoriteColor: "RED",
-    image: "https://avatars3.githubusercontent.com/u/57641436?s=400&u=1383b4d27114c6c7e3f05d55a4b4ac536971316a&v=4"
+    image:
+      "https://avatars3.githubusercontent.com/u/57641436?s=400&u=1383b4d27114c6c7e3f05d55a4b4ac536971316a&v=4",
   },
   {
     id: 3,
     name: "Trinity",
     email: "drt@drt.com",
     favoriteColor: "blue",
-    image: "https://avatars1.githubusercontent.com/u/31781724?s=460&u=10531433afe0ed55948173fab061a75fc3ffa8c1&v=4"
+    image:
+      "https://avatars1.githubusercontent.com/u/31781724?s=460&u=10531433afe0ed55948173fab061a75fc3ffa8c1&v=4",
   },
 ];
 
 team.forEach((taco) => {
-  console.log(taco.email)
+  console.log(taco.email);
 });
 
-
 for (taco of team) {
-  console.log(taco.name, taco.favoriteColor)
+  console.log(taco.name, taco.favoriteColor);
 }
+
+const div = document.querySelector("#main-btn");
+
+div.addEventListener("click", () => {
+  console.log("You Clicked Me");
+});
